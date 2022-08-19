6641976167d609da817361cd7cb383d59e0303bc
Doug Rosman <drosma@artic.edu>
2022-08-19 11:14:55 -0500

    fix contract name

--- nft-media/token-gated-nft/GatedUnlockNFT.sol
+++ nft-media/token-gated-nft/GatedUnlockNFT.sol
@@ -1,77 +1,95 @@
+/* How this code works:
+
+This code contains two smart contracts: an "Unlock" NFT (UNFT)
+and a "Gated" NFT (GNFT). The terms "Unlock" NFT and "Gated" NFT
+are made up for this exercise, and aren't pre-existing terms.
+
+The UNFT is a basic NFT, nothing special about it. The GNFT is a basic
+NFT with some custom functionality: In order to mint a GNFT, you must
+first own a UNFT. You need an "Unlock" NFT to mint the "Gated" NFT.
+
+In order to check if a minter owns a UNFT, you must call the "balanceOf"
+function from the UNFT contract in the mint function of the GNFT contract.
+
+Key concepts:
+* token-gated function interactions
+* calling one contract's functions from another contract
+*/
+
+
 // SPDX-License-Identifier: MIT
-// for more information, https://dougrosman.notion.site/Token-gated-NFT-a26dc9a1798949ff89321763414ede81
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-// Owning at least 1 UnlockNFT is required in order to mint a GatedNFT
-// This UnlockNFT contract is a generic ERC721 contract
-contract UnlockNFT is ERC721, ERC721Burnable, Ownable {
+/*
+Contract UnlockNFT is a standard ERC721 contract generated
+using the OpenZeppelin Contract Wizard. The only extra features
+added are the "Mintable" feature and Auto Increment Ids feature.
+The point is, there is nothing special about the Unlock NFT itself.
+ */
+contract UnlockNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-
+    
     constructor() ERC721("UnlockNFT", "UNFT") {}
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 }
 
-// A GatedNFT cannot be minted unless the minter owns an Unlock NFT
-contract GatedNFT is ERC721, ERC721Burnable, Ownable {
+/*
+Contract GatedNFT includes standard ERC721 functionality, but
+with some custom code in the mint() function that requires a minter
+to own at least one UNFT before they are able to mint a GNFT.
+*/
+contract GatedNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+
+    // a state variable to store the unlock NFT contract address
+    address private _UNFTContractAddress;
 
     constructor() ERC721("GatedNFT", "GNFT") {}
 
-    // adding the UnlockNFT _unlockNFT parameter to the function. This requires
-    // the person minting the NFT to manually insert the UnlockNFT contract address
-    // when they call the function. Alternatively, the address could be hard-coded into
-    // the contract, but I guess that doesn't make things that much easier, since you
-    // still have to deploy one contract, grab the address, then put it in the other
-    // contract. 
+    function safeMint(address to) public {
 
-    // Contracts are a lot like classes. The functions of a contract become methods.
+        // make sure the unlock NFT contract address has been set
+        require(_UNFTContractAddress != 0x0000000000000000000000000000000000000000,
+                "The UnlockNFT contract address has not been set"
+                );
+        
+        UnlockNFT _unftContract = UnlockNFT(_UNFTContractAddress);
+        // check that the minter's balance of UNFT is greater than 1
+        require(_unftContract.balanceOf(msg.sender) > 0,
+                "Need an UnlockNFT in order to mint"
+                );
 
-    // the address to the contract you're importing is passed in when the function is called.
-    function safeMint(UnlockNFT _unlockNFT, address to) public onlyOwner {
-        require(unlockNFT.balanceOf(msg.sender) > 0, "Need an UnlockNFT in order to mint");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
-    // alternative version 1, the contract address is hardcoded into the contract.
-    // function safeMint(address to) public onlyOwner {
+    // sets the unlock NFT contract address
+    function setUnlockNFTSContractAddress(address _contractAddress)
+        public
+        onlyOwner {
+        _UNFTContractAddress = _contractAddress;
+    }
 
-    //     // store the UnlockNFT contract in a variable called unlockNFT. The 'UnlockNFT' must
-    //     // precisely match the name of the contract you're importing (including case)
-    //     // note: The UnlockNFT contract must be deployed first in order to get the address
-    //     UnlockNFT unlockNFT = UnlockNFT(0xe2899bddFD890e320e643044c6b95B9B0b84157A);
-
-    //     // call the balanceOf() function from the unlockNFT contract to check the minter's
-    //     // balance of unlockNFT. If they have at least 1, they can mint.
-    //     require(unlockNFT.balanceOf(msg.sender) > 0, "Need an UnlockNFT in order to mint");
-    //     uint256 tokenId = _tokenIdCounter.current();
-    //     _tokenIdCounter.increment();
-    //     _safeMint(to, tokenId);
-    // }
-
-    // alternative version 2, where the address to the contract you're importing is passed in
-    // when the function is called, and passed in as an address explicitly, instead of as
-    // a contract
-    // function safeMint(address contractAddress, address to) public onlyOwner {
-    //     UnlockNFT unlockNFT = UnlockNFT(contractAddress)
-    //     require(unlockNFT.balanceOf(msg.sender) > 0, "Need an UnlockNFT in order to mint");
-    //     uint256 tokenId = _tokenIdCounter.current();
-    //     _tokenIdCounter.increment();
-    //     _safeMint(to, tokenId);
-    // }
+    // returns the address of the unlock NFT contract
+    function getUnlockNFTSContractAddress()
+        public
+        view
+        returns (address) {
+        return _UNFTContractAddress;
+    }
 }
+
